2a2e8611426661d9c2e19e897d047fab5969581e
potemove <116613092+potemove@users.noreply.github.com>
2025-05-25 11:26:59 +0800

    Update sever.js

--- 某Q音sign参数生成/sever.js
+++ 某Q音sign参数生成/sever.js
@@ -1,16 +1,16 @@
 // NodeJs架设本地服务端
 
-const chunk = require('./vendor.chunk.js');
+const loader = require('./loader');
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({extended: false}));  // 设置参数解析中间件
+app.use(express.urlencoded({extended: false}));
 
 app.post('/encrypt/qq/music', (req, res) => {
     let data = req.body.data;
     console.log(data);
 // 调用加密参数生成逻辑
-    let result = chunk.encrypt(data);
+    let result = loader.encrypt(data);
     console.log(result);
 // 返回结果
     res.send(result);
